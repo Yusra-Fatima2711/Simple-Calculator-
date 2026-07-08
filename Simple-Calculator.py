7f81397673f06a7b97dc55bfc953c2a459366ce3
Yusra Fatima <yusrafatima.professional@gmail.com>
2026-07-08 08:00:19 +0530

    Create Simple-Calculator.py

---
+++ Simple-Calculator.py
@@ -0,0 +1,17 @@
+Code:
+
+# Variables storing numerical value
+num1 = float(input("Enter first number."))
+num2 = float(input("Enter second number."))
+
+# Mathematical Operations 
+
+addition = num1 + num2
+substraction = num1 - num2 
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f"the sum is: {addition}")
+print(f"the difference is:{substraction}")
+print(f"the product is: {multiplication}")
+print(f"the quotient is: {division}")
